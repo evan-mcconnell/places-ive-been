fb0a367c7fcc252d74b5829699a020d7b4d9361d
Kimberly McConnell <kmcconnell393@gmail.com>
2019-02-06 15:27:45 -0800

    fix issue with creating unique IDs

--- js/scripts.js
+++ js/scripts.js
@@ -6,19 +6,29 @@ function AllPlaces() {
 }
 
 AllPlaces.prototype.addPlace = function(place) {
+  place.id = this.assignID();
   this.places.push(place);
 }
 
 AllPlaces.prototype.assignID = function() {
-  this.currentID ++;
+  this.currentID += 1;
   return this.currentID;
+}
+
+AllPlaces.prototype.findPlace = function(id) {
+  for(var i=0; i<this.places.length; i++) {
+    if (this.places[i].id === id) {
+      return this.places[i];
+    }
+    return false;
+  }
 }
 
 function Place(city,country,stay,review) {
   this.city = city,
   this.country = country,
   this.stay = stay,
-  this.review = review,
+  this.review = review
 }
 
 Place.prototype.percentageOfLife = function (stay) {
@@ -28,14 +38,19 @@ Place.prototype.percentageOfLife = function (stay) {
 
 //UI logic
 $(document).ready( function() {
+  var allPlaces = new AllPlaces();
   $("#place-form").submit( function(event) {
     event.preventDefault();
     var inputtedCity = $("#city").val();
     var inputtedCountry = $("#country").val();
     var inputtedStay = $("#stay-length").val();
     var inputtedReview = $("#review").val();
+
+    var newPlace = new Place(inputtedCity, inputtedCountry, inputtedStay, inputtedReview);
+    allPlaces.addPlace(newPlace);
+    console.log(newPlace);
+    console.log(allPlaces.findPlace(2));
   });
-  
 
 
 
