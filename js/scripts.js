2c431358dce67d38411e12dd5f293db142cbe567
Kimberly McConnell <kmcconnell393@gmail.com>
2019-02-06 16:44:32 -0800

    add lookup and deletion of places prototypes

--- js/scripts.js
+++ js/scripts.js
@@ -17,11 +17,25 @@ AllPlaces.prototype.assignID = function() {
 
 AllPlaces.prototype.findPlace = function(id) {
   for(var i=0; i<this.places.length; i++) {
-    if (this.places[i].id === id) {
-      return this.places[i];
+    if (this.places[i]) {
+      if (this.places[i].id === id) {
+        return this.places[i];
+      }
     }
-    return false;
-  }
+  };
+  return false;
+}
+
+AllPlaces.prototype.deletePlace = function(id) {
+  for(var i=0; i<this.places.length; i++) {
+    if (this.places[i]) {
+      if (this.places[i].id === id) {
+        delete this.places[i];
+        return true;
+      }
+    }
+  };
+  return false;
 }
 
 function Place(city,country,stay,review) {
@@ -35,7 +49,6 @@ Place.prototype.percentageOfLife = function (stay) {
   return stay / userLife * 100;
 }
 
-
 //UI logic
 $(document).ready( function() {
   var allPlaces = new AllPlaces();
@@ -48,7 +61,13 @@ $(document).ready( function() {
 
     var newPlace = new Place(inputtedCity, inputtedCountry, inputtedStay, inputtedReview);
     allPlaces.addPlace(newPlace);
+
+
+
+
+
     console.log(newPlace);
+    console.log(allPlaces);
     console.log(allPlaces.findPlace(2));
   });
 
